2d2be3de87102b115a4cb5c9aab0ab4d133fa060
selim <selim-z@bitbucket.org>
2020-07-01 17:07:32 +0200

    fianl

--- functions/post.js
+++ functions/post.js
@@ -18,7 +18,7 @@ export async function handler(event, context) {
   const contact_prenom = params.contact_prenom || "ERROR";
   try {
 
-    const response = await axios.get(`http://dev.cartegriseminute.net/apiecom/pointDeVente/${id}/finliserInscription`, { headers: { Accept: "application/json",
+    const response = await axios.put(`http://dev.cartegriseminute.net/apiecom/partenaire/pointDeVente/${id}/finliserInscription`, { headers: { Accept: "application/json",
                                                                                                                                                     Authorization: "eUs0ejBhem1zd3huVEdJUElBd2Y4anhrWWFVTjJtSnhKUEFHc0hKRXkyZkVtS2ZMVWpqakF2bTMwVzdJa1IvSHpmL0NEWk14TUxMdmFuQWdncTZacmk5RFU5WUpnVUppOVpmUmUvT0I4TjZyVVlvMFJRM2Z5Y0ZNdkRrVmRXNjFSNFk3alVESW10eUFWSTljRWMyNTdTK2NieWpUUVFUR0hEZkwxM3A3UGFHMWdzN3JIRytUR3JYMVNuc1BYSTZTMVltWFVDMUNrU2NmZThna1NZTFhyRmhlZGZBSHFjVzhldHZuTVMxSW9kSEcrZmFpbEFlWTU3VlpXTjB1elhYQXRYd2tVYjhhVitlOHVlcVpyUHJsRXM0enpJdWlqblMyZi9pMzdqejVhYnBMSk9GdTRmbkRlUzc4SlR2SEwySHRGY3RJMGVLMS9taitlTGhwMnJQZEl4by8wbFhEOERldzRzVFhydGp5OHd5d2tpSHE1bmMzd2RhbXE0bUNCM3hNZUtRVEhHaVNTdmlXM003cWdid1pFWnR0T2dNUWpFYjZCVC9EQzgvOUpUczNHOEVRUTZ3VEdMaS95bmVGMlR5QWJxU3ZOYlEzaUJBK2lNUEpTMUZObTg5dmJ1Y2VRemZqZHFlbUpyVStWcFhzNnJHQzRxNFcyQU1VZ0xaRXc0R3hLUGtrQlhEYWRoSytrN1pBWkYzVnk3QTJyY1lkU0d4NE1tUzdFUHdHYnVUbWo0ZGRmWFgyeFo2b3FaclJTWnRTWFZYckt5Wm5IQSt2Y3d1VmE2bFBLdDllVHpKSlhENVNwODlLZ0RtdGFjdWNIRVp1cHZ1bXhyUkMxUXRwS1JyMERUblFMdndFQU1DOE4zUmJDbGNpNXRQS3pIUHJLVmlmWi9uQnIrMnBTNVRJV1BxbHhoZzdlTHpDZlVoTnYvMzg=" } })
     const data = response.data
     return {
